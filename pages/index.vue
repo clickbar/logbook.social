11170915d3c424d42db08c9588a15a9cafe1e4d3
Alexander von Studnitz <alexander.von.studnitz@gmail.com>
2020-03-22 21:49:33 +0100

    Noch ein Update

--- pages/index.vue
+++ pages/index.vue
@@ -683,23 +683,24 @@
         >
           Wir brauchen deine Unterstützung.
           <br />
-          Like unseren Devpost und unser Youtube-Video.
+          Like unser Youtube-Video.
         </h2>
         <div class="flex justify-center mt-8">
           <div class="inline-flex rounded-md shadow">
             <a
-              href="https://devpost.com/software/08_coronatracking_socialcontacttrackingapp"
+              href="#email"
               class="inline-flex items-center justify-center px-5 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:shadow-outline"
             >
-              Jetzt für uns abstimmen!
+              E-Mail-Adresse eintragen
             </a>
           </div>
           <div class="inline-flex ml-3">
             <a
-              href="#email"
+              href="https://www.youtube.com/watch?v=xRn-b24aogM"
+              target="_blank"
               class="inline-flex items-center justify-center px-5 py-3 text-base font-medium leading-6 text-indigo-700 transition duration-150 ease-in-out bg-indigo-100 border border-transparent rounded-md hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline focus:border-indigo-300"
             >
-              E-Mail-Addresse eintragen
+              Youtube-Video liken
             </a>
           </div>
         </div>
@@ -794,7 +795,7 @@
                   <p
                     class="text-5xl font-extrabold leading-none text-indigo-600"
                   >
-                    72
+                    48
                   </p>
                   <p class="mt-2 text-lg font-medium leading-6 text-gray-500">
                     Stunden
